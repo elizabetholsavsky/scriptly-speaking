849dcba84cff67a731d240ce80769ffa7e2c10db
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-30 13:36:33 -0500

    move clog to utils

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
-const { clog } = require('./middleware/clog');
+const { clog } = require('./utils/clog');
 const helpers = require('./utils/helpers');
 
 // import db connection
