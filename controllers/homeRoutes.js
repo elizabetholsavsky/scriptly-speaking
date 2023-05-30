fe1ffee355091488299ea4b5605dbf5bcb55422a
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-30 12:08:34 -0500

    style login and signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,22 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/new', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
