1f569d9274b2035a3ce35db19766ecd99adb8fbc
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-05-31 13:43:06 -0500

    add login route to api/user file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,33 @@
 const router = require('express').Router();
 const { User, Post, Comment, Update } = require('../../models');
 
+// Login 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username }});
+
+        if(!userData) {
+            res.status(400).json({ message: "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again." });
+            return;
+        }
+
+        const validatePassword = await userData.checkPassword(req.body.password);
+
+        if(!validatePassword) {
+            res.status(400).json({ message: "We're sorry, but the login information you provided is not valid. Please double-check your username and password and try again." });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            req.session.username = userData.username;
+            res.json({ user: userData, message: "You're in, welcome!" })
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
+
 module.exports = router;
