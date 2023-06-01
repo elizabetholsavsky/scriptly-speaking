923131622b66a8feed5995c96089b5c525e484fc
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-01 14:47:28 -0500

    add metadata and title to main handlebars page

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,7 +8,7 @@ num_of_comments: (comments) => {
 
     switch (comments.length) {
         case 0:
-            return "Leave a comment...";
+            return "Leave a comment";
         case 1:
             return `${comments.length} comment`;
         default:
