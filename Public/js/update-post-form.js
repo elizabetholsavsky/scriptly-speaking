519cf311596042fcd1e46272f6f9f172da067fad
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-03 15:26:10 -0500

    post route added but not updated new text--yet

--- Public/js/update-post-form.js
+++ Public/js/update-post-form.js
@@ -8,18 +8,47 @@ const showUpdateForm = (event) => {
     updateCard.innerHTML = `
         <form #update-post-form">
             <label for="title">Title</label> <br>
-            <input type="text" id="new-post-title" value=${originalTitle}> <br>
+            <input type="text" id="update-post-title" value=${originalTitle}> <br>
 
             <label for="body">Body</label> <br>
-            <textarea id="new-post-body">${originalText}</textarea> <br>
+            <textarea id="update-post-body">${originalText}</textarea> <br>
 
             <div class="btn-container">
-                <button type="submit" class="submit-btn">Update</button>
+                <button type="submit" class="submit-btn" id="send-update-btn">Update</button>
             </div>
         </form>
     `;
 
-    document.querySelector('#update-post-form').addEventListener('submit', newPost);
+    document.getElementById('send-update-btn').addEventListener('click', updatePost);
+
+    const updatePost = async (event) => {
+        event.preventDefault();
+    
+        const title = document.getElementById('update-post-title').value.trim();
+        const post_body = document.getElementById('update-post-body').value.trim();
+        const url = window.location.toString().split('/');
+        const post_id = url[url.length - 1];
+    
+        if (comment_body) {
+            const response = await fetch(`/api/post/${post_id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    title,
+                    post_body,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+    
+            if (response.ok) {
+                document.location.reload();
+            } else {
+                alert(response.statusText);
+            }
+        }
+    };
 }
 
 document.getElementById('update-btn').addEventListener('click', showUpdateForm);
+
