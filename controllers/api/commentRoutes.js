0f976355649c7bc59bf7aa9b06df4bcce84c4d46
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-04 13:57:54 -0500

    add delete comment route, delete comment btn, and delete comment js handler

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -17,4 +17,23 @@ router.post('/', async (req, res) => {
     }
 });
 
+// delete comment
+router.delete('/:id', async (req, res) => {
+    try {
+        const comment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }); 
+        console.log(comment);
+        if(!comment) {
+        res.status(404).json({ message: 'No comment found with that id!' });
+        return;
+        }
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
