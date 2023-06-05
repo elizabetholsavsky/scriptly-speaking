7c9718813ad9a62fcd1c3d8609590e644a23f2c5
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-05 09:22:39 -0500

    move path for static file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const path = require('path');
 const { clog } = require('./utils/clog');
 const helpers = require('./utils/helpers');
 
