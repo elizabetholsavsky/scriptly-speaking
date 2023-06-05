f254e3d4ebc834b1fa1ee246be81660ca0ba0593
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-05 11:01:06 -0500

    revert previous change

--- Public/js/newPost.js
+++ Public/js/newPost.js
@@ -2,7 +2,7 @@ async function newPost(event) {
     event.preventDefault();
 
     const title = document.querySelector('#new-post-title').value.trim();
-    const post_body = document.querySelector('#new-post-body').value;
+    const post_body = document.querySelector('#new-post-body').value.trim();
 
     if (title && post_body) {
         const response = await fetch(`/api/post`, {
