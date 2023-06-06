4e7a7f0034fcd2365687616130c8071fe9d3ca0b
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-06 13:12:03 -0500

    create basic modal in hbs partial, create function to showModal, replace alert in login.js with function OpenAlertModal

--- Public/js/login.js
+++ Public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
         if (response.ok) {
         document.location.replace('/');
         } else {
-            alert('Check username and/or password and try again.');
+            openAlertModal();
         }
     }
 };
